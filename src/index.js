a00bfb21259f52a7837e40c3349dedc0518ed0b2
Anir Jung Thapa <anir234thapa@gmail.com>
2023-06-22 11:34:51 +0545

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>hello world</h1>);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>hello world</h1>
+);
+
+
